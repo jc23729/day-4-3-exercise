268128f90444d9cbdba6fcaae8c3d114a4394516
Juan Cruz <jc23729@gmail.com>
2020-12-04 19:02:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+#input always takes in string
+#if you wanted column2 row 3 it would be #2, #3
+horizontal= position[0]#2
+vertical= position[1]#3
+
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
